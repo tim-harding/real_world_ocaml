d8d4b1e14f83ea18eb19725e1ff2785222968d11
Tim Harding <tim@timharding.co>
2023-10-22 13:33:37 -0700

    Chapter 1

--- bin/main.ml
+++ bin/main.ml
@@ -1,1 +1,10 @@
-let () = print_endline "Hello, World!"
+open Base
+open Stdio
+
+let rec read_and_accumulate accum =
+  let line = In_channel.input_line In_channel.stdin in
+  match line with
+  | None -> accum
+  | Some x -> read_and_accumulate (accum +. Float.of_string x)
+
+let () = printf "Total: %F\n" (read_and_accumulate 0.)
