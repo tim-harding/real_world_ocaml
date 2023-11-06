9810e7a434c157fc29d574eb6e632ac7f25ef7c9
Tim Harding <tim@timharding.co>
2023-11-05 16:34:34 -0800

    Initial printfs

--- bin/main.ml
+++ bin/main.ml
@@ -1,5 +1,1 @@
-(* open Base *)
-(* open Stdio *)
-(* open Real_world_ocaml *)
-
 let () = Real_world_ocaml.Guided_tour.hello ()
--- lib/guided_tour.ml
+++ lib/guided_tour.ml
@@ -1,1 +1,6 @@
-let hello () = print_endline "Hello, world!"
+open Stdio
+
+let hello () =
+  let _ = printf "%d\n" (3_000 + 4) in
+  let _ = printf "%F\n" (3.5 +. 6.) in
+  ()
